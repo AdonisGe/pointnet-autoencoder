983ccae7baa2c3a8fbfb22ce5571b29652af8812
WeihangWANG <wangweihang_china@163.com>
2020-09-20 19:08:05 +0800

    remove unused firles

--- models/model_mk.py
+++ models/model_mk.py
@@ -5,14 +5,12 @@ Author: Charles R. Qi
 Date: May 2018
 """
 import tensorflow as tf
-import tensorflow_compression as tfc
 import numpy as np
 import math
 import sys
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(BASE_DIR)
-sys.path.append(BASE_DIR)
 sys.path.append(os.path.join(ROOT_DIR, 'utils'))
 import tf_util
 sys.path.append(os.path.join(ROOT_DIR, 'tf_ops/nn_distance'))
@@ -24,7 +22,7 @@ def placeholder_inputs(batch_size, num_point):
     return pointclouds_pl, labels_pl
 
 
-def encoder(point_cloud, is_training, bn_decay=None):
+def get_model(point_cloud, is_training, bn_decay=None):
     """ Autoencoder for point clouds.
     Input:
         point_cloud: TF tensor BxNx3
@@ -34,63 +32,66 @@ def encoder(point_cloud, is_training, bn_decay=None):
         net: TF tensor BxNx3, reconstructed point clouds
         end_points: dict
     """
-    with tf.variable_scope() as sc:
-        batch_size = point_cloud.get_shape()[0].value
-        num_point = point_cloud.get_shape()[1].value
-        point_dim = point_cloud.get_shape()[2].value
-        end_points = {"num_point":num_point,"batch_size":batch_size}
+    batch_size = point_cloud.get_shape()[0].value
+    num_point = point_cloud.get_shape()[1].value
+    point_dim = point_cloud.get_shape()[2].value
+    end_points = {}
 
-        input_image = tf.expand_dims(point_cloud, -1)
+    input_image = tf.expand_dims(point_cloud, -1)
 
-        # Encoder
-        net = tf_util.conv2d(input_image, 64, [1,point_dim],
+    # Encoder
+    net = tf_util.conv2d(input_image, 64, [1,point_dim],
                          padding='VALID', stride=[1,1],
                          bn=True, is_training=is_training,
                          scope='conv1', bn_decay=bn_decay)
-        net = tf_util.conv2d(net, 64, [1,1],
+    net = tf_util.conv2d(net, 64, [1,1],
                          padding='VALID', stride=[1,1],
                          bn=True, is_training=is_training,
                          scope='conv2', bn_decay=bn_decay)
-        point_feat = tf_util.conv2d(net, 64, [1,1],
+    point_feat = tf_util.conv2d(net, 64, [1,1],
                          padding='VALID', stride=[1,1],
                          bn=True, is_training=is_training,
                          scope='conv3', bn_decay=bn_decay)
-        net = tf_util.conv2d(point_feat, 128, [1,1],
+    net = tf_util.conv2d(point_feat, 128, [1,1],
                          padding='VALID', stride=[1,1],
                          bn=True, is_training=is_training,
                          scope='conv4', bn_decay=bn_decay)
-        net = tf_util.conv2d(net, 1024, [1,1],
+    net = tf_util.conv2d(net, 1024, [1,1],
                          padding='VALID', stride=[1,1],
                          bn=True, is_training=is_training,
                          scope='conv5', bn_decay=bn_decay)
-        global_feat = tf_util.max_pool2d(net, [num_point,1],
+    global_feat = tf_util.max_pool2d(net, [num_point,1],
                                      padding='VALID', scope='maxpool')
 
-        net = tf.reshape(global_feat, [batch_size, -1])
-       
+    net = tf.reshape(global_feat, [batch_size, -1])
+    end_points['embedding'] = net
+
+    # Entropy bottleneck
+    entropy_bottleneck = EntropyBottleneck()
+    end_points['entropy_bottleneck'] = entropy_bottleneck
+
+    net, likelihoods = entropy_bottleneck(net, training=is_training)
+    end_points['likelihoods'] = likelihoods
+
+    # FC Decoder
+    net = tf_util.fully_connected(net, 1024, bn=True, is_training=is_training, scope='fc1', bn_decay=bn_decay)
+    net = tf_util.fully_connected(net, 1024, bn=True, is_training=is_training, scope='fc2', bn_decay=bn_decay)
+    net = tf_util.fully_connected(net, num_point*3, activation_fn=None, scope='fc3')
+    net = tf.reshape(net, (batch_size, num_point, 3))
+
     return net, end_points
 
-def decoder(net, is_training, end_points,bn_decay=None):
-	num_point = end_points["num_point"]
-	batch_size = end_points["batch_size"]
-    with tf.variable_scope() as sc:
-        net = tf_util.fully_connected(net, 1024, bn=True, is_training=is_training, scope='fc1', bn_decay=bn_decay)
-        net = tf_util.fully_connected(net, 1024, bn=True, is_training=is_training, scope='fc2', bn_decay=bn_decay)
-        net = tf_util.fully_connected(net, num_point*3, activation_fn=None, scope='fc3')
-        net = tf.reshape(net, (batch_size, num_point, 3))
-    return net, end_points
-        
 def get_loss(pred, label, end_points):
     """ pred: BxNx3,
         label: BxNx3, """
     dists_forward, _, dists_backward, _ = tf_nndistance.nn_distance(pred, label)
-    bits = tf.reduce_sum(tf.log(likelihoods), axis=(1,2)) / -np.log(2) / end_points["num_point"]
-    bits = tf.reduce_mean(bits)
+    likelihoods = end_points['likelihoods']
+    bits = tf.reduce_sum(tf.log(likelihoods), axis=(1, 2, 3)) / -np.log(2)
     
     loss = tf.reduce_mean(dists_forward+dists_backward)
-    main_loss = 0.5 * loss + bits
-    end_points['pcloss'] = main_loss
-    return main_loss, end_points
+    main_loss = 0.5 * tf.reduce_mean(squared_error) + tf.reduce_mean(bits)
+    end_points['pcloss'] = loss
+    return loss*100, end_points
 
 
 if __name__=='__main__':
